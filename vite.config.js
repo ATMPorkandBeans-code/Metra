51035d415647b7b18594add92b72149ad8d4e93c
Ryan Palermo <ryan@ryanpalermo.dev>
2026-02-05 09:14:26 -0500

    chore: remove local testing changes

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/Metra/',
   plugins: [react()],
-  server: {
-    allowedHosts: ['localhost', '127.0.0.1', 'ff00f61a11ac.ngrok.app']
-  }
 })
